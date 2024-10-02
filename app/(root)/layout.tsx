9419753b582cb5d2b9d8525157fe59bddc427d5b
AjayTumula <ajaytumula72@gmail.com>
2024-10-02 11:16:43 +0530

    structure the screen layout, add rightsidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import LeftSidebar from "@/components/ui/LeftSidebar";
+import RightSidebar from "@/components/ui/RightSidebar";
+import Image from "next/image";
 
 export default function RootLayout({
   children,
@@ -6,11 +8,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div>
-        <main>
+    <div className="relative flex flex-col">
+        <main className="relative flex bg-black-3">
             <LeftSidebar />
-            {children}
-            <p className="text-white-1">RIGHT SIDEBAR</p>
+
+            <section className="border-2 border-red-500 flex min-h-screen
+             flex-1 flex-col px-4 sm:px-14">
+              <div>
+                <div>
+                  {/* <Image /> */}
+                  MobileNav
+                </div>
+                <div>
+                  Toaster (Notification Popups)
+                  {children}
+                </div>
+              </div>
+            </section>
+
+           <RightSidebar />
         </main>
     </div>
   );
