c3023b806e6b56bfdd64209c1e16505d3645000a
AjayTumula <ajaytumula72@gmail.com>
2024-10-04 11:19:23 +0530

    add webhook, users db table logic

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+//https://fond-jaguar-80.clerk.accounts.dev
+
+export default {
+    providers: [
+      {
+        domain: "https://fond-jaguar-80.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
---
+++ convex/http.ts
@@ -0,0 +1,5 @@
+// create a clerk user -> info -> user.create()
+
+// https://fond-jaguar-80.clerk.accounts.dev
+
+
